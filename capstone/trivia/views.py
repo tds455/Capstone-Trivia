5ea9431e62edf9270c254e7d6bd567b9c0369a7d
tds455 <tds-1993@outlook.com>
2022-04-07 16:33:40 +0900

    Django setup, created initial templates

--- capstone/trivia/views.py
+++ capstone/trivia/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import json
 
 # Create your views here.
+def index(request):
+    # Return the front page
+    return render(request, "index.html")
+
+
+def test(request):
+    return HttpResponse("fdfdsfsdfsdfsdf")
