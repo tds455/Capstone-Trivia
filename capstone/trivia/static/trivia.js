916084679d9963feae65e5d20e1c5a0b7025f652
tds455 <tds-1993@outlook.com>
2022-04-09 20:49:33 +0900

    Added APItest function

--- capstone/trivia/static/trivia.js
+++ capstone/trivia/static/trivia.js
@@ -3,7 +3,8 @@ document.addEventListener('DOMContentLoaded', function() {
     // document.querySelector('#newpost-form').addEventListener('submit', create_post);
 
     document.getElementById("triviaform").addEventListener("click", quizview);
-    defaultview()
+    document.getElementById("triviasubmit").addEventListener("click", apitest);
+    quizview()
     });
 
 function defaultview() {
@@ -14,4 +15,8 @@ function defaultview() {
 function quizview() {
     document.querySelector('#defaultview').style.display = 'none';
     document.querySelector('#quizview').style.display = 'block';
+}
+
+function apitest() {
+
 }
