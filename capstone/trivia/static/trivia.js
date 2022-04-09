2ccb0aa9f6bb54970f42b49ccdf28df7b3aeba6e
tds455 <tds-1993@outlook.com>
2022-04-09 20:48:04 +0900

    Added trivia form

--- capstone/trivia/static/trivia.js
+++ capstone/trivia/static/trivia.js
@@ -1,6 +1,17 @@
 document.addEventListener('DOMContentLoaded', function() {
 
     // document.querySelector('#newpost-form').addEventListener('submit', create_post);
-    
-    listposts()
+
+    document.getElementById("triviaform").addEventListener("click", quizview);
+    defaultview()
     });
+
+function defaultview() {
+    document.querySelector('#defaultview').style.display = 'block';
+    document.querySelector('#quizview').style.display = 'none';
+}
+
+function quizview() {
+    document.querySelector('#defaultview').style.display = 'none';
+    document.querySelector('#quizview').style.display = 'block';
+}
